b277a2e6c7bb67e19dadcbe708e87ababe224a37
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-03-17 09:54:04 -0700

    updatetodo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
       <h3>Todos</h3>
+      <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+      </div>
       <div class="todos">
-          <div v-for="todo in allTodos" :key="todo.id" class="todo">
+          <div 
+            @dblclick="onDnlClick(todo)" 
+            v-for="todo in allTodos" 
+            :key="todo.id" 
+            class="todo"
+            :class="{'is-complete':todo.completed}"
+          >
               {{ todo.title }}
               <i @click="deleTodo(todo.id)" class="fa fa-trash-alt"></i>
           </div>
@@ -15,7 +30,16 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
     name: 'Todos',
     methods: {
-        ...mapActions(['fetchTodos', 'deleteTodo'])
+        ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+        onDblClick(todo) {
+          const updTodo = {
+            id: todo.id,
+            title: todo.title,
+            completed: !todo.completed
+          }
+
+          this.updateTodo(updTodo)
+        }
     },
     computed: mapGetters(['allTodos']),
     created() {
